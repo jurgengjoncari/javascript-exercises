e73313721a67603f12dffb9f49a826e24e303f4c
Jurgen <jurgen.gjoncari@gmail.com>
2021-03-01 08:50:32 +0100

    Solve leapYears

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,6 +1,14 @@
 const leapYears = function(year) {
+    // Short solution: 
+    // return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
     if (year % 4 == 0) {
-        if (year % 100 != 0 && year % 400 == 0) return true;
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
+            return false;
+        } 
+        return true;
     }
     return false;
 }
